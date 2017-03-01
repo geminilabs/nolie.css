901bb32bbbe12884fff129e4f38cc037d878e5ef
Paul Ryley <paul@geminilabs.io>
2017-03-01 12:03:24 -0400

    fix node_modules imports with sass-module-importer

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var browserSync  = require( 'browser-sync' ).create();
-var gulp         = require( 'gulp' );
-var autoprefixer = require( 'gulp-autoprefixer' );
-var cssnano      = require( 'gulp-cssnano' );
-var gulpif       = require( 'gulp-if' );
-var insert       = require( 'gulp-insert' );
-var notify       = require( 'gulp-notify' );
-var rename       = require( 'gulp-rename' );
-var sass         = require( 'gulp-sass' );
-var watch        = require( 'gulp-watch' );
-var args         = require( 'yargs' ).argv;
+var browserSync    = require( 'browser-sync' ).create();
+var gulp           = require( 'gulp' );
+var autoprefixer   = require( 'gulp-autoprefixer' );
+var cssnano        = require( 'gulp-cssnano' );
+var gulpif         = require( 'gulp-if' );
+var insert         = require( 'gulp-insert' );
+var notify         = require( 'gulp-notify' );
+var rename         = require( 'gulp-rename' );
+var sass           = require( 'gulp-sass' );
+var watch          = require( 'gulp-watch' );
+var moduleImporter = require( 'sass-module-importer' );
+var args           = require( 'yargs' ).argv;
 
 var paths = {
 	src: ['src/**/*.scss', 'demo/**/*.scss'],
@@ -20,7 +21,10 @@ gulp.task( 'css', function ()
 {
 	return gulp
 		.src( paths.src, { base: '.' })
-		.pipe( sass({ outputStyle: 'expanded' }).on( 'error', sass.logError ))
+		.pipe( sass({
+			importer: moduleImporter(),
+			outputStyle: 'expanded'
+		}).on( 'error', sass.logError ))
 		.pipe( autoprefixer() )
 		.pipe( gulpif( args.production, cssnano({
 			discardComments: { removeAll: true }
