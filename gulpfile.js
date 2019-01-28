01ed79e8473d53bbba7a2bcbabe83134347229db
Paul Ryley <paul@geminilabs.io>
2019-01-28 11:46:41 -0700

    v0.2.1
    - Changed package URL
    - Updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,51 @@
-var browserSync    = require( 'browser-sync' ).create();
-var gulp           = require( 'gulp' );
-var autoprefixer   = require( 'gulp-autoprefixer' );
-var cssnano        = require( 'gulp-cssnano' );
-var gulpif         = require( 'gulp-if' );
-var insert         = require( 'gulp-insert' );
-var notify         = require( 'gulp-notify' );
-var rename         = require( 'gulp-rename' );
-var sass           = require( 'gulp-sass' );
-var watch          = require( 'gulp-watch' );
-var moduleImporter = require( 'sass-module-importer' );
-var args           = require( 'yargs' ).argv;
+var args           = require('yargs').argv;
+var autoprefixer   = require('gulp-autoprefixer');
+var browserSync    = require('browser-sync').create();
+var cssnano        = require('gulp-cssnano');
+var gulp           = require('gulp');
+var gulpif         = require('gulp-if');
+var insert         = require('gulp-insert');
+var moduleImporter = require('sass-module-importer');
+var notify         = require('gulp-notify');
+var pump           = require('pump');
+var rename         = require('gulp-rename');
+var sass           = require('gulp-sass');
+var watch          = require('gulp-watch');
 
 var paths = {
-	src: ['src/**/*.scss', 'demo/**/*.scss'],
+    src: ['src/**/*.scss', 'demo/**/*.scss'],
 };
 
-/* CSS Task
- -------------------------------------------------- */
-gulp.task( 'css', function ()
-{
-	return gulp
-		.src( paths.src, { base: '.' })
-		.pipe( sass({
-			importer: moduleImporter(),
-			outputStyle: 'expanded'
-		}).on( 'error', sass.logError ))
-		.pipe( autoprefixer() )
-		.pipe( gulpif( args.production, cssnano({
-			discardComments: { removeAll: true }
-		})))
-		.pipe( insert.prepend( '/*! nolie | MIT License | https://github.com/geminilabs/nolie.css */' ))
-		.pipe( rename( function( path ) {
-			path.dirname = path.dirname.replace( 'src', 'dist' );
-		}))
-		.pipe( gulp.dest( '.' ))
-		.pipe( browserSync.stream() )
-		.pipe( notify({
-			message: 'CSS Task complete!',
-			onLast : true
-		}));
+gulp.task('css', function(cb) {
+    pump([
+        gulp.src(paths.src, {base:'.'}),
+        sass({
+            importer: moduleImporter(),
+            outputStyle: 'expanded',
+        }).on('error', sass.logError),
+        autoprefixer(),
+        gulpif(args.production, cssnano({
+            discardComments: {removeAll: true}
+        })),
+        insert.prepend('/*! nolie | MIT License | https://github.com/pryley/nolie.css */'),
+        rename(function(path) {
+            path.dirname = path.dirname.replace('src', 'dist');
+        }),
+        gulp.dest('.'),
+        browserSync.stream(),
+        notify({
+            message: 'CSS Task complete!',
+            onLast : true
+        }),
+    ], cb);
 });
 
-/* Watch Task
- -------------------------------------------------- */
-gulp.task( 'watch', function ()
-{
-	browserSync.init({
-		server: {
-			baseDir: "demo"
-		}
-	});
-
-	gulp.watch( paths.src, ['css'] );
-	gulp.watch( 'demo/*.html' ).on( 'change', browserSync.reload );
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {baseDir: 'demo'},
+    });
+    gulp.watch(paths.src, gulp.parallel('css'));
+    gulp.watch('demo/*.html').on('change', browserSync.reload);
 });
 
-/* Default Task
- -------------------------------------------------- */
-gulp.task( 'default', function ()
-{
-	gulp.start( 'css' );
-});
+gulp.task('default', gulp.series('css'));
